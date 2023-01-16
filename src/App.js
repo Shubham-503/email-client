d868addb284b83a7c25d12146c08f048f1c6bca8
Shubham-503 <kr.shubham025@gmail.com>
2023-01-16 11:08:02 +0530

    Added Email List using API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Email from './components/Email';
 import EmailActive from './components/EmailActive';
 
 function App() {
+  const [emails, setEmails] = useState([])
+
+ const fetchEmailsbyPage= async(page) => {
+   axios.get(`${process.env.REACT_APP_API_URL}?page=${page}`)
+    .then(res=>{
+      setEmails(res.data.list)
+    })
+ }
+
+ useEffect(() => {
+  fetchEmailsbyPage(1)
+ 
+   
+ }, [emails])
+ 
+
   return (
     <div className="app">
       <div className="emails">
-
-      <Email />
-      <Email />
+        {
+          emails.map((email)=>{
+           return <Email email_data={email} key={email.id}/>
+          })
+        }
+        {/* <Email />
+        <Email /> */}
       </div>
-      <EmailActive/>
+      <EmailActive />
     </div>
   );
 }
--- src/components/Email.js
+++ src/components/Email.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./Email.css"
 
-const Email = () => {
+const Email = ({email_data: email}) => {
   return (
-    <div className='email email--read' onClick={()=>{console.log('clicked')}}>
+    <div className='email email--read' onClick={()=>{
+      console.log('clicked')
+      console.log(process.env.REACT_APP_API_URL)
+      console.log(process.env)
+    }}>
       <div className="email__left">
-        <span className="email__avatar">A</span>
+        <span className="email__avatar">{email.from?.name.split("")[0].toUpperCase()}</span>
       </div>
       <div className="email__right">
-        <p className="email__from">From: <strong>Foo Bar foo.bar@email.com</strong></p>
-        <p className="email__subject">Subject: <strong>Lorem ipsum</strong></p>
-        <p className="email__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, dolorum!</p>
+        <p className="email__from">From: <strong>{email.from?.name}  {email.from?.email}</strong></p>
+        <p className="email__subject">Subject: <strong>{email.subject}</strong></p>
+        <p className="email__description">{email.short_description}</p>
         <p className="email__dateTime">26/02/22 10:30am <span className='email--fav'>Favourite</span></p>
       </div>
     </div>
