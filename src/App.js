3952ac2b97c85a6e6d3ac15744f46fdfab648a8f
Shubham-503 <kr.shubham025@gmail.com>
2023-01-16 22:04:35 +0530

    Added active  read and unread in Ui

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import EmailActive from './components/EmailActive';
 function App() {
   const [emails, setEmails] = useState([])
   const [activeEmail, setActiveEmail] = useState({})
+  const [activeEmailId, setActiveEmailId] = useState(-1)
 
  const fetchEmailsbyPage= async(page) => {
    axios.get(`${process.env.REACT_APP_API_URL}?page=${page}`)
     .then(res=>{
-      setEmails(res.data.list)
+      setEmails(res.data.list.map((email)=>{
+          email.unread= true;
+          email.favorite = false;
+          email.active = false
+          return email
+      }))
     })
  }
 
@@ -25,7 +31,7 @@ function App() {
       <div className="emails" style={{width:(Object.keys(activeEmail).length!==0?"30%":"100%") }} >
         {
           emails.map((email)=>{
-           return <Email email_data={email} key={email.id} setActiveEmail={setActiveEmail} activeEmail={activeEmail}/>
+           return <Email email_data={email} key={email.id} setActiveEmail={setActiveEmail} activeEmail={activeEmail} setActiveEmailId={setActiveEmailId} activeEmailId={activeEmailId}/>
           })
         }
         {/* <Email />
--- src/components/Email.js
+++ src/components/Email.js
@@ -3,8 +3,9 @@ import { useRef } from 'react';
 import React, { useEffect, useState } from 'react'
 import "./Email.css"
 
-const Email = ({email_data: email, setActiveEmail, activeEmail}) => {
+const Email = ({email_data, setActiveEmail, activeEmail}) => {
   const [date, setDate] = useState("")
+  const [email, setEmail] = useState(email_data)
   const emailRef = useRef(null);
 
   useEffect(() => {
@@ -16,22 +17,26 @@ const Email = ({email_data: email, setActiveEmail, activeEmail}) => {
     axios.get(`${process.env.REACT_APP_API_URL}?id=${id}`)
     .then(res=>{
       setActiveEmail({
+        id:email.id,
         subject: email.subject,
         name:email.from?.name,
         date:date,
         body:res.data.body
       })
       console.log(emailRef)
-      emailRef.current.className += "email--active"
+      setEmail({...email,unread:false,active:true})
+      email.unread=false;
+      
     })
   }
 
   return (
-    <div className={`email ${activeEmail && email.id === activeEmail.id ? "email--active": "" }`}  onClick={()=>{
-      fetchEmail(email.id)
-      console.log('clicked', email.id, activeEmail.id)
-    }}
-    ref= {emailRef}
+    <div className={`email ${email.id===activeEmail.id?"email--active":""} ${email.unread?"email--unread":""} `}  
+      onClick={()=>{
+        fetchEmail(email.id)
+        console.log('clicked', email, activeEmail.id)
+      }}
+      ref= {emailRef}
     >
       <div className="email__left">
         <span className="email__avatar">{email.from?.name.split("")[0].toUpperCase()}</span>
