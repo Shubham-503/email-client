43e8225e3c49078afe4e5b74cfdd9c04aab4551d
Shubham-503 <kr.shubham025@gmail.com>
2023-01-16 17:09:10 +0530

    Added emailActive on click

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Email from './components/Email';
 import EmailActive from './components/EmailActive';
 
 function App() {
   const [emails, setEmails] = useState([])
+  const [activeEmail, setActiveEmail] = useState({})
 
  const fetchEmailsbyPage= async(page) => {
    axios.get(`${process.env.REACT_APP_API_URL}?page=${page}`)
@@ -16,23 +17,21 @@ function App() {
 
  useEffect(() => {
   fetchEmailsbyPage(1)
- 
-   
  }, [emails])
  
 
   return (
     <div className="app">
-      <div className="emails">
+      <div className="emails" style={{width:(Object.keys(activeEmail).length!==0?"30%":"100%") }} >
         {
           emails.map((email)=>{
-           return <Email email_data={email} key={email.id}/>
+           return <Email email_data={email} key={email.id} setActiveEmail={setActiveEmail} activeEmail={activeEmail}/>
           })
         }
         {/* <Email />
         <Email /> */}
       </div>
-      <EmailActive />
+       { Object.keys(activeEmail).length!==0 &&  <EmailActive activeEmail={activeEmail}  />}
     </div>
   );
 }
--- src/components/Email.js
+++ src/components/Email.js
@@ -1,21 +1,46 @@
-import React from 'react'
+import axios from 'axios'
+import { useRef } from 'react';
+import React, { useEffect, useState } from 'react'
 import "./Email.css"
 
-const Email = ({email_data: email}) => {
+const Email = ({email_data: email, setActiveEmail, activeEmail}) => {
+  const [date, setDate] = useState("")
+  const emailRef = useRef(null);
+
+  useEffect(() => {
+    let email_date = new Date(email.date)
+    setDate(`${String(email_date.getDate()).padStart(2,"0")}/${String(email_date.getMonth()).padStart(2,"0")}/${String(email_date.getFullYear()).padStart(2,"0")} `)
+  })
+  
+  const fetchEmail = (id) => {
+    axios.get(`${process.env.REACT_APP_API_URL}?id=${id}`)
+    .then(res=>{
+      setActiveEmail({
+        subject: email.subject,
+        name:email.from?.name,
+        date:date,
+        body:res.data.body
+      })
+      console.log(emailRef)
+      emailRef.current.className += "email--active"
+    })
+  }
+
   return (
-    <div className='email email--read' onClick={()=>{
-      console.log('clicked')
-      console.log(process.env.REACT_APP_API_URL)
-      console.log(process.env)
-    }}>
+    <div className={`email ${activeEmail && email.id === activeEmail.id ? "email--active": "" }`}  onClick={()=>{
+      fetchEmail(email.id)
+      console.log('clicked', email.id, activeEmail.id)
+    }}
+    ref= {emailRef}
+    >
       <div className="email__left">
         <span className="email__avatar">{email.from?.name.split("")[0].toUpperCase()}</span>
       </div>
       <div className="email__right">
-        <p className="email__from">From: <strong>{email.from?.name}  {email.from?.email}</strong></p>
+        <p className="email__from">From: <strong>{email.from?.name} &#60;{email.from?.email}&#62;</strong></p>
         <p className="email__subject">Subject: <strong>{email.subject}</strong></p>
         <p className="email__description">{email.short_description}</p>
-        <p className="email__dateTime">26/02/22 10:30am <span className='email--fav'>Favourite</span></p>
+        <p className="email__dateTime">26/02/22 10:30am {date} <span className='email--fav'>Favourite</span></p>
       </div>
     </div>
   )
